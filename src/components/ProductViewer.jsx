c16fd45ec53cbe48cb92386f21f4caa9eef58591
0098-sayan <maitysayan704@gmail.com>
2026-04-14 23:59:15 +0530

    Added model switcher

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -1,20 +1,24 @@
-import clsx from "clsx";
 import useMacbookStore from "../store";
+import clsx from "clsx";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
-import MacbookModel14 from "./models/Macbook-14";
-import StudioLights from "./StudioLights";
+
+
+import StudioLights from "./three/StudioLights.jsx";
+import ModelSwitcher from "./three/ModelSwitcher.jsx";
+import { useMediaQuery } from "react-responsive";
 
 const ProductViewer = () => {
   const { color, scale, setColor, setScale } = useMacbookStore();
 
+  const isMobile = useMediaQuery({ query: "(max-width: 1024px)" });
+
   return (
     <section id="product-viewer">
-      <h2>Take a closure look</h2>
+      <h2>Take a closer look.</h2>
+
       <div className="controls">
-        <p className="info">
-          MacbookPro {scale} in {color}
-        </p>
+        {/*<p className="info">Macbook Pro | Available in 14" & 16" in Space Gray & Dark colors</p>*/}
+
         <div className="flex-center gap-5 mt-5">
           <div className="color-control">
             <div
@@ -32,6 +36,7 @@ const ProductViewer = () => {
               )}
             />
           </div>
+
           <div className="size-control">
             <div
               onClick={() => setScale(0.06)}
@@ -41,7 +46,7 @@ const ProductViewer = () => {
                   : "bg-transparent text-white",
               )}
             >
-              <p>14″</p>
+              <p>14"</p>
             </div>
             <div
               onClick={() => setScale(0.08)}
@@ -51,21 +56,24 @@ const ProductViewer = () => {
                   : "bg-transparent text-white",
               )}
             >
-              <p>16″</p>
+              <p>16"</p>
             </div>
           </div>
         </div>
       </div>
+
       <Canvas
         id="canvas"
         camera={{ position: [0, 2, 5], fov: 50, near: 0.1, far: 100 }}
       >
-        <StudioLights/>
-        <MacbookModel14 scale={0.06} position={[-1, 0, 0]} />
-        <OrbitControls enableZoom={false} />
+        <StudioLights />
+
+        <ModelSwitcher
+          scale={isMobile ? scale - 0.03 : scale}
+          isMobile={isMobile}
+        />
       </Canvas>
     </section>
   );
 };
-
 export default ProductViewer;
